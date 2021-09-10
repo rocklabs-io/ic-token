5797aab96903c7d00a8eccb86b03f579da5cdfc1
flyq <flyq951@gmail.com>
2021-09-10 14:42:39 +0800

    fix metadata, metainfo, rm unsafe

--- rust/deploy.sh
+++ rust/deploy.sh
@@ -1,5 +1,8 @@
 sudo dfx canister --no-wallet create --all
-cargo run token > ./token.did
+cargo run > token.did
 ic-cdk-optimizer target/wasm32-unknown-unknown/release/token.wasm -o target/wasm32-unknown-unknown/release/opt.wasm
 sudo dfx build token
-sudo dfx canister --no-wallet install erc20 --argument "(\"test token\", \"TT\", 8:nat64, 100000000:nat64)" -m=reinstall
+OWNER="principal \"$( \
+   dfx identity get-principal
+)\""
+sudo dfx canister --no-wallet install token --argument "(\"test logo\", \"test token\", \"TT\", 8:nat8, 100000000:nat64, $OWNER, true, true)" -m=reinstall
